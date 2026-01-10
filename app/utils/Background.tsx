48d8f478a85e3ef31a8dec51648b335528f64ed8
danielmazzeudev <danielmazzeu.dev@gmail.com>
2026-01-10 18:57:42 -0300

    Upload

--- app/utils/Background.tsx
+++ app/utils/Background.tsx
@@ -7,7 +7,7 @@ const BackgroundBubbles = () => {
     const [bubbles, setBubbles] = useState<Array<{id: number; size: number; left: number; top: number; color: string}>>([]);
 
     useEffect(() => {
-        const colors = ["#252c3b99", "#65cc6599"];
+        const colors = ["#252c3bcc", "#65cc65cc"];
 
         const createBubble = () => ({
             id: Math.random(),
