00775900935de9eeb32a0dbada02107ce13524bd
danielmazzeudev <danielmazzeu.dev@gmail.com>
2026-01-10 21:33:53 -0300

    Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css"; 
 
@@ -15,6 +16,7 @@ export default function RootLayout({
     return (
         <html lang="pt-br">
             <body>
+                <Analytics/>
                 {children}
             </body>
         </html>
