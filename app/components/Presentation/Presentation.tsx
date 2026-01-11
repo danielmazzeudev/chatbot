b07fb590f8d502e26652802731441d988444bed6
danielmazzeudev <danielmazzeu.dev@gmail.com>
2026-01-10 21:01:03 -0300

    Upload

--- app/components/Presentation/Presentation.tsx
+++ app/components/Presentation/Presentation.tsx
@@ -9,7 +9,7 @@ export function Presentation() {
             <Image width={100} height={100} src="/logo.png" alt="Logo Neppo" />
             <h1>NEPB<BotMessageSquare />T</h1>
             <p>
-                Nepbot is an intelligent assistant specialized in the <strong>NEPPO</strong> ecosystem.
+                NEPBOT is an intelligent assistant specialized in the <strong>NEPPO</strong> ecosystem.
                 Developed to improve processes and guide clients with maximum agility and solutions.
             </p>
             <small>
